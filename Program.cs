3265e6e7a4e3288932a49770f1ad835692174af3
Pi0trS <piotrsy82@gmail.com>
2017-10-25 20:52:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal_mandelbrot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
